eec3c497d40219921de6cf9f1ce617cda347b384
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-13 23:06:38 -0400

    loop through input userValue array and if in range of num of elements, assigns new intValue array to userValue index; printing for testing

--- OutputValuesBelowAnAmount.java
+++ OutputValuesBelowAnAmount.java
@@ -7,8 +7,16 @@ public class OutputValuesBelowAnAmount {
       int NUM_ELEMENTS = scnr.nextInt();
       System.out.println("NUM_ELEMENTS: " + NUM_ELEMENTS);
       int[] userValues = new int[20];  
+      int[] intValues = new int[NUM_ELEMENTS];
       
-      
+      for(int i = 0; i < NUM_ELEMENTS; ++i){
+        userValues[i] = scnr.nextInt();
+
+        if(i < NUM_ELEMENTS){
+            System.out.println(userValues[i]);
+            intValues[i] = userValues[i];
+        }
+      }
 
       
    }
