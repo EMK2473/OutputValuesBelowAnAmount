4d9b50de874ccaf24f026641f6636cfb0917e103
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-13 23:24:45 -0400

    removed testing printouts; formatted for submission;

--- OutputValuesBelowAnAmount.java
+++ OutputValuesBelowAnAmount.java
@@ -3,9 +3,7 @@ import java.util.Scanner;
 public class OutputValuesBelowAnAmount {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
-
       int NUM_ELEMENTS = scnr.nextInt();
-      System.out.println("NUM_ELEMENTS: " + NUM_ELEMENTS);
       int[] userValues = new int[20];  
       int[] intValues = new int[NUM_ELEMENTS];
       
@@ -13,22 +11,16 @@ public class OutputValuesBelowAnAmount {
         userValues[i] = scnr.nextInt();
 
         if(i < NUM_ELEMENTS){
-            System.out.println(userValues[i]);
             intValues[i] = userValues[i];
         }
       }
       
       for(int i = 0; i < intValues.length; ++i){
-        System.out.println("NUM OF ELELEMTS " + NUM_ELEMENTS);
         if (intValues[i] < userValues[NUM_ELEMENTS]){
-           System.out.println("IntegerValues: ");
-           System.out.println(intValues[i] + ",");
+           System.out.print(intValues[i] + ",");
         }
       }
-      int threshold = userValues[NUM_ELEMENTS];
       System.out.println();
-      System.out.print("Threshold: " + threshold);
-
-      
+      scnr.close();
    }
 }
