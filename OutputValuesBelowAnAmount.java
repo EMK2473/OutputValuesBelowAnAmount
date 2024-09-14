00541ca183e48bee2d97e87cbd4f28db08f7bb3c
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-13 20:31:36 -0400

    init file

---
+++ OutputValuesBelowAnAmount.java
@@ -0,0 +1,10 @@
+import java.util.Scanner; 
+
+public class OutputValuesBelowAnAmount {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int[] userValues = new int[20];   // List of integers from input
+
+      /* Type your code here. */
+   }
+}
