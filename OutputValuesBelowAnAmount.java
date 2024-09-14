8e53abb9933789672b1b4facc67ae7824881cc24
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-13 22:58:18 -0400

    assigned first input as num of elements; printed for testing

--- OutputValuesBelowAnAmount.java
+++ OutputValuesBelowAnAmount.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 public class OutputValuesBelowAnAmount {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
-      int[] userValues = new int[20];   // List of integers from input
 
-      /* Type your code here. */
+      int NUM_ELEMENTS = scnr.nextInt();
+      System.out.println("NUM_ELEMENTS: " + NUM_ELEMENTS);
+      int[] userValues = new int[20];  
+      
+      
+
+      
    }
 }
