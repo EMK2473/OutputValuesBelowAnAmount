4f0ab14db2254024505e721987d571159cc35eb1
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-14 12:48:53 -0400

    reviewed code and removed unneccessary intValue array; reassigned userValues to handle what intValue was handling; less code and more optimal

--- OutputValuesBelowAnAmount.java
+++ OutputValuesBelowAnAmount.java
@@ -4,20 +4,17 @@ public class OutputValuesBelowAnAmount {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
       int NUM_ELEMENTS = scnr.nextInt();
-      int[] userValues = new int[20];  
-      int[] intValues = new int[NUM_ELEMENTS];
+      int[] userValues = new int[NUM_ELEMENTS];  
       
-      for(int i = 0; i < NUM_ELEMENTS + 1; ++i){
+      for(int i = 0; i < NUM_ELEMENTS; ++i){
         userValues[i] = scnr.nextInt();
-
-        if(i < NUM_ELEMENTS){
-            intValues[i] = userValues[i];
-        }
       }
       
-      for(int i = 0; i < intValues.length; ++i){
-        if (intValues[i] < userValues[NUM_ELEMENTS]){
-           System.out.print(intValues[i] + ",");
+      int threshold = scnr.nextInt();
+
+      for(int i = 0; i < userValues.length; ++i){
+        if (userValues[i] < threshold){
+           System.out.print(userValues[i] + ",");
         }
       }
       System.out.println();
